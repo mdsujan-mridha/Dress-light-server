4455ffec8d42095a26970b99e2289786b6292850
mdsujan-mriddha <mdsujan134523@gmail.com>
2024-09-11 13:33:35 +0600

    create product api

--- controller/productController.js
+++ controller/productController.js
@@ -64,5 +64,44 @@ exports.getProductDetails = catchAsyncErrors(async (req, res, next) => {
     });
 });
 
+//delete product
+exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+        return next(new ErrorHandler("Product Not Found", 404));
+    }
+    await product.deleteOne();
+    res.status(200).json({
+        success: true,
+        message: "Product Deleted Successfully",
+    });
+});
+//update product
+exports.updateProduct = catchAsyncErrors(async (req, res, next) => {
+    let product = await Product.findById(req.params.id);
+    if (!product) {
+        return next(new ErrorHandler("Product Not Found", 404));
+    }
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false,
+    });
+    res.status(200).json({
+        success: true,
+        product,
+        message: "Product Updated Successfully",
+    });
+});
+// get all product by admin 
+exports.getAllProductsAdmin = catchAsyncErrors(async (req, res, next) => {
+    const products = await Product.find();
+    res.status(200).json({
+        success: true,
+        products,
+        message:"All Products Fetched Successfully",
+    });
+});
+
 
 
