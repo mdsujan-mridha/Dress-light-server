474c7c7b5014c5b4ab765b3d472761d2052e6ad3
mdsujan-mriddha <mdsujan134523@gmail.com>
2024-11-07 22:49:55 +0600

    add example

---
+++ utils/example.js
@@ -0,0 +1,26 @@
+// PORT = 5000
+// JWT_SECRET=sujan
+// JWT_EXPIRE=5d
+// DB_PASS=X7OEAiyHkXgVosah
+// DB_NAME=nextbdsite3
+// COOKIE_EXPIRE = 5
+
+// CLOUD_NAME = dhnmtvmgh
+// CLOUD_API_KEY = 772424568111986
+// CLOUD_API_SECRET = NmhIvILWmdrDr07CiQh7Kjt2Yus
+
+
+// SMTP_SERVICE=gmail
+
+// SMTP_MAIL=nextbdsite1@gmail.com
+
+// appPassword=nuzewoyxyvwfwddw
+
+// SMTP_HOST=smtp.gmail.com
+
+// SMTP_PORT=465 
+
+// FRONTEND_URL = http://localhost:5173
+
+// STRIPE_SECRET_KEY=sk_test_51Lewq3IIcDCQ7G4Ui1Ei1NfifOMauYSJeE0TehsV78qOGsLxvqGepE7AtDvDYd3wnhJXAdARLOuUZaI0u2ZDB5gd005z8yiDNY
+// STRIPE_API_KEY=pk_test_51Lewq3IIcDCQ7G4UoASctUrWcyL2cRiTLtvgQXRMWmSdwPQtXOSskCv5ILZ9PEqY3Jos8uSezpduNluexAfJEKsQ00REtuIxSE
