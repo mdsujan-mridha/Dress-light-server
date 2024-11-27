675e22ffedd58ad83299f6582ae67f850dc73206
mdsujan-mriddha <mdsujan134523@gmail.com>
2024-11-27 11:25:53 +0600

    update app

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = require("./app");
 const database = require("./config/dbConnect");
 const dotenv = require("dotenv");
 const port = process.env.PORT || 5000;
-const cloudinary = require("cloudinary");
+// const cloudinary = require("cloudinary");
+const cloudinaryModule = require("cloudinary");
+const { v2: cloudinary } = cloudinaryModule;
 
 // handle uncaught Exception 
 process.on("uncaughtException", err => {
@@ -21,11 +23,22 @@ dotenv.config({ path: "./config/config.env" });
 database();
 
 // cloudinary config 
+// cloudinary.config({
+//     cloud_name: process.env.CLOUD_NAME,
+//     api_key: process.env.CLOUD_API_KEY,
+//     api_secret: process.env.CLOUD_API_SECRET
+// })
+// cloudinary config 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUD_API_KEY,
     api_secret: process.env.CLOUD_API_SECRET
-})
+});
+// cloudinaryModule.config({
+//     cloud_name: process.env.CLOUD_NAME,
+//     api_key: process.env.CLOUD_API_KEY,
+//     api_secret: process.env.CLOUD_API_SECRET
+// })
 
 app.get("/", async (req, res) => {
     res.send("Hello Admin server is working ")
