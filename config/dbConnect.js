4bfb6782446cb9ca341e64cbfa5f31e9fb37732d
mdsujan-mriddha <mdsujan134523@gmail.com>
2025-01-23 23:06:28 +0600

    update databse\

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -8,7 +8,7 @@ const database = (module.exports = () => {
     }
     try {
         mongoose.set('strictQuery', true);
-        mongoose.connect(`mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.0w9io.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+        mongoose.connect(`mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.xmvci.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
         console.log("Database Connected ✅✅")
 
     } catch (error) {
